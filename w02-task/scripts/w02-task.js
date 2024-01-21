99103c05f8127053234cf4f0fd9d375a849c6110
Blessing Aniedi Asuwko <blessingasukwo15@gmail.com>
2024-01-21 21:16:42 +0100

    added

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -2,14 +2,21 @@
 
 /* Step 1 - Setup type tasks - no code required */
 
-/* Step 2 - Variables */
+/* Step 2 - Variables */       
+let fullName = 'Blessing Asukwo';
+let currentYear = new Date().getFullYear();
+let myImage = document.getElementById("C:/Documents/cse121b/w02-task/images/my image.jpg");
+
 
 
 
 
 /* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
 
-
+const imageElement = document.getElementById('profileImage');
 
 
 
